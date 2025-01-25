d7c8c4cebe95e418f73ed741bf1c679671aa713d
fguirama <florian.guiramand@protonmail.com>
2025-01-25 16:42:19 +0100

    make test_005_finish_disconnect_not_score and test_006_finish_disconnect_not_score

--- tests/test_game.py
+++ tests/test_game.py
@@ -201,6 +201,32 @@ class Test03_Finish(UnitTest):
 
         self.assertThread(user1, user2)
 
+    def test_005_finish_disconnect_not_score(self):
+        user1 = self.user(['game-start'])
+        user2 = self.user(['game-start'])
+
+        match_id = self.assertResponse(create_game(user1, user2), 201, get_field=True)
+        self.assertResponse(finish_match(match_id, 'player-disconnect', user1['id']), 200)
+
+        for u in (user1, user2):
+            match = self.assertResponse(get_games(u), 200, get_field='results', count=1)[0]
+            self.assertEqual(user2['id'], match['teams'][match['winner']]['players'][0]['id'])
+
+        self.assertThread(user1, user2)
+
+    def test_006_finish_disconnect_not_score(self):
+        user1 = self.user(['game-start'])
+        user2 = self.user(['game-start'])
+
+        match_id = self.assertResponse(create_game(user1, user2), 201, get_field=True)
+        self.assertResponse(finish_match(match_id, 'player-disconnect', user2['id']), 200)
+
+        for u in (user1, user2):
+            match = self.assertResponse(get_games(u), 200, get_field='results', count=1)[0]
+            self.assertEqual(user1['id'], match['teams'][match['winner']]['players'][0]['id'])
+
+        self.assertThread(user1, user2)
+
 
 class Test04_Tournament(UnitTest):
 
